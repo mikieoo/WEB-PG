da876e57924d9d2e582c9f145ec0fab7d4ea9038
mikieoo <jcw2551@naver.com>
2024-08-07 16:51:15 +0900

    first

---
+++ HTML+CSS/senicare/sign-in.js
@@ -0,0 +1,34 @@
+var userIdElement = document.getElementById('user-id');
+var userPwElement = document.getElementById('user-password');
+var messageElement = document.getElementById('message');
+var signInButton = document.getElementById('sign-in-button');
+
+function onSignInButtonClickHandler (event) {
+    var userId = userIdElement.value;
+    var userPw = userPwElement.value;
+
+    if (userId !== 'qwer1234' || userPw !== 1234) {
+        messageElement.textContent = '로그인 정보가 일치하지 않습니다.';
+        return;
+    } 
+
+    alert('로그인 성공!');
+    messageElement.textContent = '';
+     // alert(userId + ", " + userPw); 아이디랑 비밀번호가 잘들어갔는지 확인용
+}
+
+function userIdKeyPressHandler (event) {
+    if (event.key === 'Enter') {
+        userPwElement.focus();
+    }
+}
+
+function userPwKeyPressHandler (event) {
+    if (event.key === 'Enter'){
+        onSignInButtonClickHandler();
+    }
+}
+
+signInButton.addEventListener('click', onSignInButtonClickHandler);
+userIdElement.addEventListener('keypress', userIdKeyPressHandler);
+userPwElement.addEventListener('keypress', userPwKeyPressHandler);
---
+++ HTML+CSS/senicare/sign-up.js
@@ -0,0 +1,8 @@
+var userNameElement = document.getElementById('user-name');
+var userIdElement = document.getElementById('user-id');
+var userPwElement = document.getElementById('user-password');
+var userPwCheckElememt = document.getElementById('user-password-check');
+var userTelnumElement = document.getElementById('user-telnumber');
+var userAuthnumElement = document.getElementById('user-auth-number');
+
+
